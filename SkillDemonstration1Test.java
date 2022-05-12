845b06bd20bde4938fffcf0d50203b5d2c6b7ce2
Santiago-Duque <sduque@ucsd.edu>
2022-05-11 23:07:02 -0700

    Update SkillDemonstration1Test.java

--- SkillDemonstration1Test.java
+++ SkillDemonstration1Test.java
@@ -1,10 +1,12 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class SkillDemonstration1Test {
+public static void main(String[] args) {
+    public class SkillDemonstration1Test {
 
     @Test
     public void division() {
         assertEquals(3, SkillDemonstration1.divide(8,3));
     }
+}
 }
