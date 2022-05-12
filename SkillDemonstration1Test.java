c814ffe0f17e770056ba9b7673cb9c230414a8cb
Santiago-Duque <sduque@ucsd.edu>
2022-05-11 22:44:52 -0700

    added test file

---
+++ SkillDemonstration1Test.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemonstration1Test {
+
+    @Test
+    public void division() {
+        assertEquals(3, SkillDemonstration1.divide(8,3));
+    }
+}
